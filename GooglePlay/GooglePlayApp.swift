d481dff09e09f09b1e7429ea5033c5c26f8159f0
Raphael Cerqueira <raphaelfc@gmail.com>
2021-07-16 09:48:18 -0300

    Initial Commit

---
+++ GooglePlay/GooglePlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  GooglePlayApp.swift
+//  GooglePlay
+//
+//  Created by Raphael Cerqueira on 16/07/21.
+//
+
+import SwiftUI
+
+@main
+struct GooglePlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
